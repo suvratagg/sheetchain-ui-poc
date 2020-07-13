3439e7928de9fc5786b22f54fcd8276a61b22b13
manav <chiraniamanav15@gmail.com>
2020-07-13 19:26:57 +0530

    Updated with IPFS code

--- sheetchai-ui-kickstartup-siva-dev/sheetchai-ui-kickstartup-siva-dev/src/app/app.module.ts
+++ sheetchai-ui-kickstartup-siva-dev/sheetchai-ui-kickstartup-siva-dev/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './views/landing/landing.component';
 import { TeaminfoComponent } from './views/teaminfo/teaminfo.component';
+import { HttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
--- sheetchai-ui-kickstartup-siva-dev/sheetchai-ui-kickstartup-siva-dev/src/app/views/landing/landing.component.ts
+++ sheetchai-ui-kickstartup-siva-dev/sheetchai-ui-kickstartup-siva-dev/src/app/views/landing/landing.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+
+const ipfsClient = require('ipfs-http-client')
+const ipfs = ipfsClient({ host: 'localhost', port: '5001', protocol: 'http' })
+
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent implements OnInit {
+
+	uploadedFiles: File ;
+	hash: string;
 
   constructor() { }
 
@@ -16,21 +23,58 @@ export class LandingComponent implements OnInit {
   teamname:string = "team";
   commits:any = ["First commit V1", "Second commit V2", "Third commit V3"];
 
-  fileChange(args) {
+  fileChange(element) {
     console.log("File Change Achieved");
+    this.uploadedFiles = element.target.files[0];
+	  console.log(this.uploadedFiles.name);
   };
 
-  upload() {
+
+  /*upload() {
     console.log("upload pressed");
-  };
+  };*/
+
+
+	async upload(){
+      for await (const result of ipfs.add(this.uploadedFiles)){
+        this.hash = result.path;
+        console.log(result.path);
+      };
+}
 
   handleCommit() {
     console.log("commit pressed");
     this.showUpload = true;
   };
 
-  handleUpdate() {
+  async handleUpdate() {
     console.log("update pressed");
+
+    for await (const file of ipfs.cat(this.hash)){		//hash will be needed from backend
+      //console.log(file.content);
+      const blob = new Blob([file]);
+
+      const fileName = 'version1.xlsx'; 	// filename is hardcoded for now
+      if (navigator.msSaveBlob) {
+        // IE 10+
+      navigator.msSaveBlob(blob, fileName);
+      } else {
+        const link = document.createElement('a');
+      // Browsers that support HTML5 download attribute
+        if (link.download !== undefined) {
+          const url = URL.createObjectURL(blob);
+          link.setAttribute('href', url);
+          link.setAttribute('download', fileName);
+          link.style.visibility = 'hidden';
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link);
+        }
+      }
+
+
+    }
+
   };
 
 }
